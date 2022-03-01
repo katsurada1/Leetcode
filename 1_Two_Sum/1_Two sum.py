d19b535751e028c490387932ea9e0641097a4613
katsurada <kazuki1.katsurada@gmail.com>
2022-03-02 00:46:14 +0900

    add new file

---
+++ 1_Two_Sum/1_Two sum.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = defaultdict(int)
+        for i in range(len(nums)):
+            if target - nums[i] in dict:
+                return [i, dict[target - nums[i]]]
+            dict[nums[i]] = i
