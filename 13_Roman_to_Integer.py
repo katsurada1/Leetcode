d90c87744eae9d5314ce0e9151e18750da928307
katsurada <kazuki1.katsurada@gmail.com>
2022-03-09 23:56:04 +0900

    I modified the code

--- 13_Roman_to_Integer.py
+++ 13_Roman_to_Integer.py
@@ -12,12 +12,11 @@ class Solution(object):
         """
     
         roman = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-        res, i = 0, 0
+        res = 0
         for i in range(len(s)):
-            curr, nxt = s[i], s[i+1:i+2]
-            if nxt and roman[curr] < roman[nxt]:
-                res -= roman[curr]
+            if i+1 < len(s) and roman[s[i]] < roman[s[i+1]]:
+                res -= roman[s[i]]
             else:
-                res += roman[curr]
+                res += roman[s[i]]
         return res
         
