336b2cf506cb75930b2bcf05514f84752ddd5b4a
katsurada <kazuki1.katsurada@gmail.com>
2022-03-21 00:30:58 +0900

    I modified the code

--- 14_Longest_Common_Prefix.py
+++ 14_Longest_Common_Prefix.py
@@ -1,10 +1,11 @@
-# n = len(s)
-# time: O(n^2)
+# n = len(strs)
+# m = len(min(strs))
+# time: O(nm)
 # space: O(1)
 
 class Solution(object):
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        """A function to find the longest common prefix string amongst an array of strings.
+        """A function to find the longest common prefix string amongst an array of strings
         Args:
             strs (List[str]): A given an array of strings.
         Returns:
