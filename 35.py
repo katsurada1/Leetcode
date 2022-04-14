209d3164fadf0b98f20124769520133a4d4269e1
katsurada <kazuki1.katsurada@gmail.com>
2022-04-14 21:41:11 +0900

    I modified the code

--- 35.py
+++ 35.py
@@ -1,5 +1,5 @@
 # n = len(nums)
-# time:O(n)
+# time:O(log(n))
 # space:O(1)
 
 class Solution(object):
@@ -11,15 +11,16 @@ class Solution(object):
         Returns:
             int: The index if the target is found. If not, return the index where it would be if it were inserted in order.
         """
-        r=0
-        l=len(nums)-1
-        while r <= l:
-            mid = (r+l) // 2
+        r = 0
+        s = len(nums) - 1
+        while r <= s:
+            mid = (r + s) // 2
             if nums[mid] < target:
                 r = mid + 1
             elif nums[mid] == target:
                 return mid
             
             else:
-                l = mid - 1
+                s = mid - 1
         return r
+            
