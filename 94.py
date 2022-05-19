f8b5a5a6345463f6abad3e4fbfb91d2b5b9906ee
katsurada <kazuki1.katsurada@gmail.com>
2022-05-19 16:12:26 +0900

    I modified the code

--- 94.py
+++ 94.py
@@ -43,6 +43,7 @@ class Solution(object):
     #     res = []
     #     self.helper(root, res)
     #     return res
+    
     # def helper(self, root, res):
     #     if root:
     #         self.helper(root.left, res)
